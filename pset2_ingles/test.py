01e5a9022223bff06ed97506f857136631e429fa
Daniel Marculano <91577318+danielsmar@users.noreply.github.com>
2022-08-29 21:07:36 -0300

    Add files via upload

---
+++ pset2_ingles/test.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+import os
+from tkinter import Image
+import pset2
+import unittest
+
+TEST_DIRECTORY = os.path.dirname(__file__)
+
+class TestImage(unittest.TestCase):
+    def test_load(self):
+        result = pset2.Image.load('test_images/centered_pixel.png')
+        expected = pset2.Image(11, 11,
+                             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                              0, 0, 0, 0, 0, 255, 0, 0, 0, 0, 0,
+                              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        self.assertEqual(result, expected)
+
+
+class TestInverted(unittest.TestCase):
+    def test_inverted_1(self):
+        im = pset2.Image.load('test_images/centered_pixel.png')
+        result = im.inverted()
+        expected = pset2.Image(11, 11,
+                             [255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                              255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                              255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                              255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                              255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                              255, 255, 255, 255, 255, 0, 255, 255, 255, 255, 255,
+                              255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                              255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                              255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                              255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                              255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255])
+        self.assertEqual(result,  expected)
+
+    def test_inverted_2(self):
+        # REPLACE THIS from your test case from section 3.1
+        img = pset2.Image.new(4,1)
+        img.pixels = [29,89,136,200]
+        img.save("teste.png")
+        img_inverted = img.inverted()
+        img_inverted.save("teste_inverted.png")
+        self.assertTrue(img != img_inverted)
+
+    def test_inverted_images(self):
+        for fname in ('mushroom', 'twocats', 'chess'):
+            with self.subTest(f=fname):
+                inpfile = os.path.join(TEST_DIRECTORY, 'test_images', '%s.png' % fname)
+                expfile = os.path.join(TEST_DIRECTORY, 'test_results', '%s_invert.png' % fname)
+                result = pset2.Image.load(inpfile).inverted()
+                expected = pset2.Image.load(expfile)
+                self.assertEqual(result,  expected)
+
+
+class TestFilters(unittest.TestCase):
+    def test_blurred(self):
+        for kernsize in (1, 3, 7):
+            for fname in ('mushroom', 'twocats', 'chess'):
+                with self.subTest(k=kernsize, f=fname):
+                    inpfile = os.path.join(TEST_DIRECTORY, 'test_images', '%s.png' % fname)
+                    expfile = os.path.join(TEST_DIRECTORY, 'test_results', '%s_blur_%02d.png' % (fname, kernsize))
+                    input_img = pset2.Image.load(inpfile)
+                    input_img_copy = pset2.Image(input_img.width, input_img.height, input_img.pixels)
+                    result = input_img.blurred(kernsize)
+                    expected = pset2.Image.load(expfile)
+                    self.assertEqual(input_img, input_img_copy, "Be careful not to modify the original image!")
+                    self.assertEqual(result,  expected)
+
+    def test_sharpened(self):
+        for kernsize in (1, 3, 9):
+            for fname in ('mushroom', 'twocats', 'chess'):
+                with self.subTest(k=kernsize, f=fname):
+                    inpfile = os.path.join(TEST_DIRECTORY, 'test_images', '%s.png' % fname)
+                    expfile = os.path.join(TEST_DIRECTORY, 'test_results', '%s_sharp_%02d.png' % (fname, kernsize))
+                    input_img = pset2.Image.load(inpfile)
+                    input_img_copy = pset2.Image(input_img.width, input_img.height, input_img.pixels)
+                    result = input_img.sharpened(kernsize)
+                    expected = pset2.Image.load(expfile)
+                    self.assertEqual(input_img, input_img_copy, "Be careful not to modify the original image!")
+                    self.assertEqual(result,  expected)
+
+    def test_edges(self):
+        for fname in ('mushroom', 'twocats', 'chess'):
+            with self.subTest(f=fname):
+                inpfile = os.path.join(TEST_DIRECTORY, 'test_images', '%s.png' % fname)
+                expfile = os.path.join(TEST_DIRECTORY, 'test_results', '%s_edges.png' % fname)
+                input_img = pset2.Image.load(inpfile)
+                input_img_copy = pset2.Image(input_img.width, input_img.height, input_img.pixels)
+                result = input_img.edges()
+                expected = pset2.Image.load(expfile)
+                self.assertEqual(input_img, input_img_copy, "Be careful not to modify the original image!")
+                self.assertEqual(result,  expected)
+
+
+if __name__ == '__main__':
+    res = unittest.main(verbosity=3, exit=False)
